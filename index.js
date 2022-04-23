a194ed349b0f566ba7993e9370164b4cc7e96a32
Jonny Howle <jonny.howle@gmail.com>
2022-04-23 13:24:54 -0600

    commit tree 53f16e1428b4cab3db5fed2a3d29956907e21abd

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    changeText("This is really cool!");
+  });
+
+  function changeText(newText) {
+      let text = document.getElementById('text');
+      text.textContent = newText;
+  }
